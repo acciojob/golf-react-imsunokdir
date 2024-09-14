973ad3a2146aec70846e365412a3e52dbacc2ab6
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-09-14 11:34:42 +0530

    class comp logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-// const cStyle = {
-//   textAlign: "center",
-// };
-
-const App = () => {
-  const [renderChoice, setRenderChoice] = useState(false);
-  const [position, setPosition] = useState(5);
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      const playgroundWidth = document.querySelector(".playground").offsetWidth;
-      const ballWidth = 50;
-      const maxPosition = playgroundWidth - ballWidth;
-      if (e.key === "ArrowRight" || e.keyCode === 39) {
-        setPosition((prev) => {
-          return Math.min(prev + 5, maxPosition);
-        });
-      }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" },
     };
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+    this.renderChoice = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
 
-  const buttonClickHandler = () => setRenderChoice(true);
-  return (
-    <div className="playground">
-      {renderChoice ? (
-        <div
-          className="ball"
-          style={{ transform: `translateX(${position}px)` }}
-        ></div>
-      ) : (
-        <button onClick={buttonClickHandler} className="start">
-          start
-        </button>
-      )}
-    </div>
-  );
-};
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
+  handleKeyDown(event) {
+    if (event.key === "ArrowRight" || event.keyCode === 39) {
+      // Move ball 5px to the right when the right arrow is pressed
+      this.setState((prevState) => {
+        const newPos = prevState.posi + 5;
+        return {
+          posi: newPos,
+          ballPosition: { left: `${newPos}px` },
+        };
+      });
+    }
+  }
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return <button onClick={this.buttonClickHandler}>Start</button>;
+    }
+  }
+
+  // bind ArrowRight keydown event
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    // Clean up event listener when component unmounts
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
+}
 
 export default App;
